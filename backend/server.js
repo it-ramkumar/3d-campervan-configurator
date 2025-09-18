c015010cc08628de991ba0285906c97c13317b77
zain-ikram-nawaz <zainikram704@gmail.com>
2025-09-18 12:45:19 +0500

    update server

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(morganMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.set("trust proxy", 1);
 // 5️ Enable CORS
 app.use(corsMiddleware);
 
