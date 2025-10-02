d5f1582087b304eb8226182a30b23a8c5bccc42e
zain-ikram-nawaz <zainikram704@gmail.com>
2025-10-02 18:40:25 +0500

    add explore button

--- frontend/src/websiteComponents/components/HomePage/Blog/Blog.jsx
+++ frontend/src/websiteComponents/components/HomePage/Blog/Blog.jsx
@@ -96,6 +96,14 @@ export default function Blog() {
             </Link>
           ))}
         </div>
+        <div className="flex justify-center">
+
+        <Link to="/blogs" >
+          <button className="bg-[#2761FD] mt-14 text-white font-serif font-bold text-sm px-8 py-3 rounded-md transform transition-transform duration-300 hover:scale-105">
+                        Explore Blogs
+                    </button>
+        </Link>
+        </div>
       </div>
     </section>
   );
