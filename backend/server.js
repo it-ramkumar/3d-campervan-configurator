1be6901b9c775d49f93e5c4f2d64bf950e68d672
ramkumar <it@vankea.com>
2025-10-22 20:20:14 +0500

    webhok check

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.use("/api/quote", quoteRoutes);
 app.use("/api/models", modelsRoute);
 
 app.get("/auto", (req, res) => {
-  res.send("auto deployment is working...done ");
+  res.send("auto deployment is working...zain ");
 });
 app.use(errorHandler);
 app.get("/", (req, res) => {
