483fad7c2fe25529dd67aa6ffaa1ab9f37e8b48a
ramkumar <it@vankea.com>
2025-10-22 20:58:51 +0500

    cold

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.use("/api/quote", quoteRoutes);
 app.use("/api/models", modelsRoute);
 
 app.get("/auto", (req, res) => {
-  res.send("auto deployment is working...atif ");
+  res.send("auto deployment is working...cold ");
 });
 app.use(errorHandler);
 app.get("/", (req, res) => {
