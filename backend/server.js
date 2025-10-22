cddd53a1eb3401fc65b2ff2bfded483cf9f6ed8a
ramkumar <it@vankea.com>
2025-10-22 20:46:20 +0500

    clock

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.use("/api/quote", quoteRoutes);
 app.use("/api/models", modelsRoute);
 
 app.get("/auto", (req, res) => {
-  res.send("auto deployment is working...paper ");
+  res.send("auto deployment is working...clock ");
 });
 app.use(errorHandler);
 app.get("/", (req, res) => {
