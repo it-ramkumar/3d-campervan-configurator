cc87d117dac0e545d3e6d74dcab3a1b6d583d791
ramkumar <it@vankea.com>
2025-10-23 14:46:45 +0500

    add cors

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -18,7 +18,7 @@ const cors = require('cors');
 
 //for testing i have allow all links here
 const corsOptions = {
-  origin: ["http://localhost:5173","localhost:5173", "https://new.bigbearvans.com"],
+  origin: ["http://localhost:5173","localhost:5173", "https://new.bigbearvans.com","https://zain.d2qr91yoy4oomc.amplifyapp.com"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
