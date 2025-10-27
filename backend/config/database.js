bd508f9a3197a7f1bd0a8c2b8c6505cdf0ed2e20
Zain Ikram <zainikram704@gmail.com>
2025-10-27 17:10:23 +0500

    add db

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://bigbearvan4:bigbearvan4@cluster0.us0whl2.mongodb.net/Product_Content?retryWrites=true&w=majority', );
+    await mongoose.connect(process.env.MONGO_URL, );
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err.message);
