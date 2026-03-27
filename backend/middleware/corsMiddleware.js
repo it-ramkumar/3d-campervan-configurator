817551d0f88105819a8e81f4a667bb132ad280a3
Zain Ikram <it@vankea.com>
2026-03-27 09:43:22 +0500

    add 3000

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -21,10 +21,11 @@ const corsOptions = {
   origin: [
     "https://zain.d2qr91yoy4oomc.amplifyapp.com",
     "http://localhost:5173",
+    "http://localhost:3000",
     "https://new.bigbearvans.com",
     "https://bigbearvans.com",
     "https://www.bigbearvans.com",
-  
+
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Accept"],
