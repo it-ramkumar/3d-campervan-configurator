b3228518642ac69e68829a22a509d4a0673dc239
it-vankea <it@vankea.com>
2026-06-03 10:11:17 +0500

    add layout category

--- frontend/src/components/adminPanel/Portfolio/PortfolioForm.jsx
+++ frontend/src/components/adminPanel/Portfolio/PortfolioForm.jsx
@@ -306,7 +306,11 @@ export default function PortfolioForm({ setSelected }) {
               "Flagship Long Van — Montreal",
               "Layouts for Solo & Couple Travelers",
               "Layouts for Families (3–9 People)",
-              "Portfolio of Custom Builds"
+              "Portfolio of Custom Builds",
+              "Sugarloaf",
+              "Amsterdam",
+              "poptop",
+
             ].map((cat) => (
               <label key={cat} className="flex items-center gap-2">
                 <input
